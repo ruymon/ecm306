052272371d28fbfb638a1ecc88194178d821f9b2
ruymon <ruystfrancis@gmail.com>
2026-02-19 01:33:44 -0300

    feat(p1): Solve ex30

---
+++ P01/ex30.java
@@ -0,0 +1,27 @@
+/**
+ * Exercicio 30:
+ * Elaborar um programa, em linguagem Java, que utilize apenas métodos recursivos,
+ * capaz de receber, via teclado, um número inteiro, positivo, que represente o termo da
+ * série abaixo. Calcular o valor da série, ou seja, a soma de todos os valores calculados, do
+ * 1º termo até o termo digitado.
+ * S = (1/20) + (1/21) + (1/22) + (1/23) + (1/24) + ... + (1/2N-1) + (1/2N)
+ * Exemplo: Digitado o termo: 4 => S = 1 + 1/2 + 1/4 + 1/8 => S = 1.875
+ */
+
+import java.util.Scanner;
+
+public class ex30 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int termCount = scanner.nextInt();
+        System.out.println(seriesSumRecursive(termCount));
+        scanner.close();
+    }
+
+    static double seriesSumRecursive(int termCount) {
+        if (termCount <= 0) {
+            return 0;
+        }
+        return 1.0 / Math.pow(2, termCount - 1) + seriesSumRecursive(termCount - 1);
+    }
+}
